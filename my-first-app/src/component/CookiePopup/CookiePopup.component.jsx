998cee6d5b89593371716a8416f746dafece805c
Emmanouil Tsiomis <tsmanos1995@gmail.com>
2024-09-30 14:05:44 +0000

    Styling cookies notice

--- my-first-app/src/component/CookiePopup/CookiePopup.component.jsx
+++ my-first-app/src/component/CookiePopup/CookiePopup.component.jsx
@@ -7,6 +7,20 @@ import './CookiePopup.override.style';
 /** @namespace myFirstApp/Component/CookiePopup/Component */
 export class CookiePopupComponent extends SourceCookiePopupComponent {
     // TODO implement logic
+    renderCTA() {
+        return (
+            <div
+              block="CookiePopup"
+              elem="CTA"
+              onClick={ this.acceptCookies }
+              onKeyDown={ this.acceptCookies }
+              role="button"
+              tabIndex={ 0 }
+            >
+                { __('accept and continue') }
+            </div>
+        );
+    }
 }
 
 export default CookiePopupComponent;
